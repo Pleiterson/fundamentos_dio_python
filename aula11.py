2eabac8db7b24bc8375cd3ffe3810c9b692bec11
pleiterson <pleiterson@gmail.com>
2021-06-23 16:32:05 -0300

    concluindo aula11

--- aula11.py
+++ aula11.py
@@ -1,23 +1,23 @@
 
 lista = [1, 10]
 
-try:
-    arquivo = open('teste.txt', 'r')
-    texto = arquivo.read()
-    divisao = 10 / 1
-    numero = lista[1]
+# tudo que estiver dentro do try vai cair nas exceções
+try: # ocorrendo um erro na primeira linha, já para a execução dos demais códigos
+    divisao = 10 / 1 # se alterar 1 por 0, vai cair em 'ZeroDivisionError'
+    numero = lista[1] # se alterar o 1 por 3 vai cair em 'IndexError'
+    arquivo = open('teste.txt', 'r') # se alterar o nome do arquivo, vai cair em 'Exception'
+    texto = arquivo.read() # lendo o arquivo
 except ZeroDivisionError:
-    print('Não é possível realizar divisão por 0')
+    print('Não é possível realizar divisão por 0') # informa quando há uma divisão por zero
 except ArithmeticError:
-    print('Houve um erro ao realizar uma operação aritmética')
+    print('Houve um erro ao realizar uma operação aritmética') # idem ao 'ZeroDivisionError'
 except IndexError:
-    print('Erro ao acessar um indice inválido da lista')
+    print('Erro ao acessar um índice inválido da lista') # informa quando tenta acessar um índice inexistente na lista
 except Exception as ex:
-    print('erro desconhecido. Erro: {}'.format(ex))
-else:
-    print('Executa quando não ocorre exceção')
-finally:
-    print('Sempre executa')
-    print('Fechando arquivo')
-    arquivo.close()
-
+    print('Erro desconhecido. Erro: {}'.format(ex)) # qualquer erro que aparecer, vai ser armazendo no ex e mostrado ao usuário
+else: # vai executar quando não ocorrer nenhuma exceção
+    print('Executa quando não ocorre exceção') # se não encontrar nenhuma exceção (exception) vai entrar nesta mensagem
+finally: # vai executar independente do erro, não entra no bloco try
+    print('Sempre executa') # sempre vai aparecer
+    print('Fechando arquivo') # vai fechar o arquivo, mesmo dando o erros nos exceptions
+    arquivo.close() # fecha o arquivo
--- aula11_personalizar_excecao.py
+++ aula11_personalizar_excecao.py
@@ -1,20 +1,20 @@
-class Error(Exception):
-    pass
+class Error(Exception): # classe Error herdando da Exception
+    pass # classe vazia
 
-class InputError(Error):
-    def __init__(self, message):
+class InputError(Error): # classe InputError herdando da Error
+    def __init__(self, message): # construtor da classe InputError
         self.message = message
 
-while True:
+while True: # vai executar enquanto a verdade for verdade, rsrs
     try:
-        x = int(input('Entre com uma nota de 0 a 10: '))
+        x = int(input('Entre com uma nota de 0 a 10: ')) # se digitar uma letra vai gerar erro e vai cair em 'ValueError', digitando número sai do laço
         print(x)
-        if x > 10:
-            raise InputError('Nota não pode ser maior que 10')
-        elif x < 0:
+        if x > 10: # vai mostrar a mensagem se o número digitado for maior que 10
+            raise InputError('Nota não pode ser maior que 10') # com raise conseguimos forçar uma exceção, que no caso é InputError()
+        elif x < 0: # vai mostrar a mensagem se o número digitado for menor que 0
             raise InputError('Nota não pode ser menor que 0')
         break
     except ValueError:
         print('Valor inválido. Deve-se digitar apenas números.')
-    except InputError as ex:
-        print(ex)
+    except InputError as ex: # mostrando a mensagem da exceção ao usuário em ex
+        print(ex) # imprimindo ex = erro
